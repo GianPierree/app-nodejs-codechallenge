606c447237585d1fbb0989e9ae3dcfb96bed0c5d
Gian Munoz <gian0291@gmail.com>
2025-04-23 06:50:39 -0500

    feat: Se refactorizo el ms-trasaction para recibir evento desde el ms-atifraud-rules

--- ms-antifraud-rules/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ValidatorModule } from './validator/validator.module';
-
-@Module({
-  imports: [ValidatorModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
